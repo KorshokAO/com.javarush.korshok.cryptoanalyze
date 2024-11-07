9519003d0bc628cc5229759baa634fb710b00396
KorshokAO <korshokAO@gmail.com>
2024-11-07 15:14:03 +0300

    start

---
+++ src/Application.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("ШИФРОВЩИК МЕТОДОМ ЦЕЗАРЯ");
+        System.out.println("ВЫБЕРИТИ ПУНКТ МЕНЮ");
+        System.out.println("1.Шифрование с ключом");
+        System.out.println("2.Расшифрование с ключом");
+        System.out.println("3.Brute force");
+        System.out.println("0.Выход");
+
+        Scanner sc = new Scanner(System.in);
+        int j = sc.nextInt();
+        System.out.println("ВЫ ВЫБРАЛИ  " + j);
+
+        //iugui
+    }
+
+}
